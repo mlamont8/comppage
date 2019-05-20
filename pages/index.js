7518f94edbb8d78160e88283b510977a9f8c5eba
mlamont8 <mlamont8@hotmail.com>
2019-05-20 19:15:03 -0400

    added images to about on index

--- pages/index.js
+++ pages/index.js
@@ -11,19 +11,30 @@ class Index extends React.Component {
     return (
       <Layout>
         <Hero />
-        <section className="brands">
-          <h2>Brands</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
-            impedit amet minima iste autem cumque et maiores blanditiis
-            doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem
-            voluptas quod quae accusantium, ex inventore ducimus. Beatae
-            mollitia exercitationem, quam similique, consectetur ratione
-            reprehenderit delectus neque eligendi facere soluta dolor ducimus!
-          </p>
-        </section>
         <section className="aboutUs">
-          <h2>About Us</h2>
+          <h2>Best In The Business</h2>
+          <hr className="divider" />
+          <p className="section-intro">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
+            impedit amet
+          </p>
+          <div className="about-image-container">
+            <div className="about-image-left about-image">
+              <div>
+                <img src="/static/images/passionate.jpg" />
+              </div>
+              <span>Image Title</span>
+            </div>
+            <div className="about-image-right about-image">
+              <div>
+                <img src="/static/images/professional.jpg" />
+              </div>
+              <span>Image Title</span>
+            </div>
+          </div>
+        </section>
+        <section className="benefits">
+          <h2>benefits</h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
             impedit amet minima iste autem cumque et maiores blanditiis
@@ -39,10 +50,27 @@ class Index extends React.Component {
             padding: 20px 0;
             text-align: center;
           }
+          .about-image-container {
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+          }
+          .about-image-container div {
+            width: 500px;
+          }
 
-          .aboutUs {
+          .about-image span {
+            text-align: center:
+          }
+
+          .benefits {
             background: #333;
             color: #fff;
+          }
+
+          img {
+            width: 100%;
+            height: auto;
           }
         `}</style>
       </Layout>
