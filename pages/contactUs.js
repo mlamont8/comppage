2d282ba60dde95192ad1f8de15997302a716b709
mlamont8 <mlamont8@hotmail.com>
2019-05-29 19:33:39 -0400

    initial setup of contact page

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,9 +1,89 @@
+import React from "react";
 import Layout from "../components/Layout.js";
 
-export default function ContactUs() {
-  return (
-    <Layout>
-      <p>Contact Us Page</p>
-    </Layout>
-  );
+class ContactUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      company: "",
+      email: "",
+      comments: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section className="contactHead">
+          <h1>We'll Love To Hear From You</h1>
+          <p>
+            We've done some amazing things for our clients. Let us do something
+            much better for you.
+          </p>
+          <address>
+            Our Company
+            <br />
+            123 Address Lane <br />
+            Someplace, St 00000
+            <br />
+            (999)999-9999
+          </address>
+        </section>
+
+        <section className="inputForm">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">name</label>
+            <input
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+
+            <label htmlFor="company">company</label>
+            <input
+              name="company"
+              type="text"
+              value={this.state.company}
+              onChange={this.handleChange}
+            />
+
+            <label htmlFor="email">email</label>
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+
+            <label htmlFor="comments">comments</label>
+            <input
+              name="comments"
+              type="text"
+              value={this.state.comments}
+              onChange={this.handleChange}
+            />
+          </form>
+        </section>
+      </Layout>
+    );
+  }
 }
+
+export default ContactUs;
